85da2d9297203b7615ccb3f4cc72c6286081d477
Kyrowin <ten.ben2010@mail.ru>
2025-02-09 20:00:51 +0500

    add Yandex module

---
+++ Yandex.py
@@ -0,0 +1,31 @@
+from telethon.tl.types import Message  # type: ignore
+
+from .. import loader, utils
+
+
+@loader.tds
+class Yandex(loader.Module):
+    strings = {
+        "name": "Yandex",
+        "Yandex": (
+            "<emoji document_id=5300882244842300470>👩‍💻</emoji><b> I yandexed for"
+            " you</b>\n"
+        ),
+        "no_args": "❌ No args",
+    }
+    strings_ru = {
+        "Yandex": (
+            "<emoji document_id=5300882244842300470>👩‍💻</emoji><b> Я заяндексил за"
+            " тебя</b>\n"
+        ),
+        "no_args": "❌ Нет аргументов",
+    }
+
+    async def yandexcmd(self, message: Message):
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, self.strings("no_args"))
+            return
+        g = args.replace(" ", "+")
+        yandex = f"https://yandex.ru/search/?text={g}"
+        await utils.answer(message, self.strings("yandex") + yandex)
